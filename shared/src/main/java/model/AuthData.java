aa123d7241b8c18cd91e189a39a353b8bfdc0a87
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-05-22 08:14:51 -0600

    authData class with constructor, variables, getters and setters

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,4 +1,30 @@
 package model;
 
 public class AuthData {
+    private String authToken;
+    private String username;
+
+    //constructor to create auth data
+    public AuthData(String authToken, String username) {
+        this.authToken = authToken;
+        this.username = username;
+    }
+
+    //GETTERS
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public String username() {
+        return username;
+    }
+
+    //SETTERS
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public void setUsername() {
+        this.username = username;
+    }
 }
