97859e5e259de9ca17ebcfe88742bc95713b5826
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-05-22 08:15:15 -0600

    added missing getters/setters

--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -12,6 +12,7 @@ public class UserData {
         this.email = email;
     }
 
+    //GETTERS
     public String getUsername() {
         return username;
     }
@@ -24,6 +25,7 @@ public class UserData {
         return email;
     }
 
+    //SETTERS
     public void setPassword(String password) {
         this.password = password;
     }
