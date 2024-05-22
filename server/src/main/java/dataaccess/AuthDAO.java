e68a98adbf63ec76c5bce1bba804afd47ea2755b
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-05-22 10:51:02 -0600

    added clear, createAuth, getAuth, and deleteAuth methods with auth map

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,37 @@
 package dataaccess;
 
+
+import model.AuthData;
+import model.GameData;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AuthDAO {
+    private Map<String, AuthData> auths = new HashMap<>();
+
+    public void clear() {
+        auths.clear();
+    }
+
+    public void createAuth(AuthData auth) throws DataAccessException{
+        if(auths.containsKey(auth.getAuthToken())){
+            throw new DataAccessException("Auth token already exists.");
+        }
+        auths.put(auth.getAuthToken(), auth);
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException{
+        if(!auths.containsKey(authToken)) {
+            throw new DataAccessException("Auth token not found.");
+        }
+        return auths.get(authToken);
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException{
+        if(!auths.containsKey(authToken)) {
+            throw new DataAccessException("Auth token not found.");
+        }
+        auths.remove(authToken);
+    }
 }
