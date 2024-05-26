c7f1e0a3652ed15cbd39dbe8e278f259c9d7fcd2
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-05-25 22:38:15 -0600

    started unit test

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,4 +1,35 @@
 package service;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import service.UserService;
+import dataaccess.UserDAO;
+import dataaccess.AuthDAO;
+import model.UserData;
+import model.AuthData;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 public class UserServiceTest {
+    private UserService userService;
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+
+    @BeforeAll
+    public void setUp() {
+        userDAO = new UserDAO();
+        authDAO = new AuthDAO();
+        userService = new UserService(userDAO, authDAO);
+    }
+
+    @Test
+    public void testRegisterSuccess() throws Exception {
+        UserData user = new UserData("testUser", "password123", "test@example.com");
+        AuthData auth = userService.register(user);
+
+        assertNotNull(auth);
+        assertEquals("testUser", auth.getUsername());
+    } // go over what this means
 }
