58d170c49beabbfd762b33265cf8f7c323937d71
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-05-25 20:54:07 -0600

    finished gamehandler class

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -21,6 +21,7 @@ public class GameHandler {
         this.gameService = gameService;
     }
 
+    // list games
     public Route listGames =  (Request req, Response res) -> {
         String authtoken = req.headers("Authorization");
         try {
@@ -28,9 +29,49 @@ public class GameHandler {
             res.status(200);
             return gson.toJson(games);
         } catch (DataAccessException e) {
-            res.status(401)
+            res.status(401);
+            return "{\"message\" : \"Error: unauthorized\"}";
+        }
+    };
+
+    // create games
+    public Route createGame = (Request req, Response res) -> {
+        String authtoken = req.headers("Authorization");
+        String gameName = gson.fromJson(req.body(), String.class);
+        try {
+            GameData game = gameService.createGame(authtoken, gameName);
+            res.status(200);
+            return gson.toJson(game);
+        } catch (DataAccessException e) {
+            res.status(400);
+            return "{\"message\" : \"Error: bad request\"}";
+        }
+    };
+
+    // join game
+    public Route joinGame = (Request req, Response res) -> {
+        String authtoken = req.headers("Authorization");
+        JoinGameRequest joinRequest = gson.fromJson(req.body(), JoinGameRequest.class);
+        try {
+            gameService.joinGame(authtoken, joinRequest.getGameID(), joinRequest.getPlayerColor();
+            res.status(200);
+            return "{}";
+        } catch (DataAccessException e) {
+            res.status(403);
+            return "{\"message\" : \"Error: already taken\"}";
+        }
+    };
+
+    private static class JoinGameRequest {
+        private String playerColor;
+        private int gameID;
+
+        public String getPlayerColor() {
+            return playerColor;
         }
 
+        public int getGameID() {
+            return gameID;
+        }
     }
-
 }
