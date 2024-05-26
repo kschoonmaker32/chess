86cd4a77f2ed9e01be35a105d859ffc053603750
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-05-25 21:38:28 -0600

    unused imports

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,12 +1,8 @@
 package server;
 
-import dataaccess.DataAccessException;
 import dataaccess.GameDAO;
 import dataaccess.AuthDAO;
 import dataaccess.UserDAO;
-
-import server.UserHandler;
-import server.GameHandler;
 
 import service.UserService;
 import service.GameService;
