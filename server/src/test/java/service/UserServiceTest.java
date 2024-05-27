1c6060bb8e9038e776901b9020327eb6b16468f8
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-05-27 14:41:16 -0600

    added register failure test

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,8 +1,9 @@
 package service;
 
+import dataaccess.DataAccessException;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import service.UserService;
 import dataaccess.UserDAO;
 import dataaccess.AuthDAO;
 import model.UserData;
@@ -17,7 +18,7 @@ public class UserServiceTest {
     private UserDAO userDAO;
     private AuthDAO authDAO;
 
-    @BeforeAll
+    @BeforeEach
     public void setUp() {
         userDAO = new UserDAO();
         authDAO = new AuthDAO();
@@ -32,4 +33,11 @@ public class UserServiceTest {
         assertNotNull(auth);
         assertEquals("testUser", auth.getUsername());
     } // go over what this means
+
+    @Test //(expected = DataAccessException.class)
+    public void testRegisterUserAlreadyTaken() throws Exception {
+        UserData user = new UserData("testUser", "password123", "test@example.com");
+        userService.register(user);
+        userService.register(user); // should throw data access exception here
+    }
 }
