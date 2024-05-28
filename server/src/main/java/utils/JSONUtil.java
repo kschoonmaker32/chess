72003643b77251664e8ca8e962749f94cb758f31
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-05-27 19:04:20 -0600

    added jsonutil class

---
+++ server/src/main/java/utils/JSONUtil.java
@@ -0,0 +1,15 @@
+package utils;
+
+import com.google.gson.Gson;
+
+public class JSONUtil {
+    private static final Gson gson = new Gson();
+
+    public static <T> T fromJson(String json, Class<T> classOfT) {
+        return gson.fromJson(json, classOfT);
+    }
+
+    public static String toJson(Object src) {
+        return gson.toJson(src);
+    }
+}
