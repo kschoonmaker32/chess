b854011f41024e5d9c2ceb94028b97e3adf40203
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-05-31 14:41:19 -0600

    added list update and clear function

--- server/src/main/java/dataaccess/GameDAOMySQL.java
+++ server/src/main/java/dataaccess/GameDAOMySQL.java
@@ -7,11 +7,13 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import chess.ChessGame;
-import utils.JSONUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameDAOMySQL implements GameDAO{
 
-    public GameDAOMySQL() {}
+    //public GameDAOMySQL() {}
 
     @Override
     public void createGame(GameData game) throws DataAccessException {
@@ -49,6 +51,57 @@ public class GameDAOMySQL implements GameDAO{
                  }
              } catch (SQLException e) {
                  throw new DataAccessException("Error querying game from database");
+        }
+    }
+
+    @Override
+    public List<GameData> listGames() throws DataAccessException { // figure out exception here
+        String sql = "SELECT * FROM games";
+        List<GameData> games = new ArrayList<>();
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt  = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                games.add(new GameData(
+                    rs.getInt("gameID"),
+                    rs.getString("whiteUsername"),
+                    rs.getString("blackUsername"),
+                    rs.getString("gameName"),
+                    deserializeGame(rs.getString("gameState"))
+                ));
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error listing games from database");
+        }
+        return games;
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        String sql = "UPDATE games SET whiteUsername = ?, blackUsername = ?, gameName = ?, gameState = ? WHERE gameID = ?";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt  = conn.prepareStatement(sql)) {
+            stmt.setString(1, game.getWhiteUsername());
+            stmt.setString(1, game.getBlackUsername());
+            stmt.setString(1, game.getGameName());
+            stmt.setString(1, serializeGame(game.getGame()));
+            stmt.setInt(1, game.getGameID());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error updating game in database");
+        }
+
+        // find out why sometimes result and sometimes not
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        String sql = "DELETE FROM games";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error clearing games from database");
         }
     }
 
