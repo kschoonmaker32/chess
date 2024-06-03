3c016362e9910e23b0c3ff554dccab1a77831cbf
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-03 13:29:51 -0600

    finished initializedatabase method with create tables functionality

--- server/src/main/java/dataaccess/DatabaseInitializer.java
+++ server/src/main/java/dataaccess/DatabaseInitializer.java
@@ -17,14 +17,23 @@ public class DatabaseInitializer {
             String createUsersTable = "CREATE TABLE IF NOT EXISTS users (" +
                     "username VARCHAR(255) PRIMARY KEY," +
                     "email VARCHAR(255) NOT NULL, " +
-                    "password VARCHAR(255) NOT NULL";
+                    "password VARCHAR(255) NOT NULL)";
             try (PreparedStatement stmt = conn.prepareStatement(createUsersTable)) {
                 stmt.executeUpdate();
             }
 
             // create games table here
             String createGamesTable = "CREATE TABLE IF NOT EXISTS games (" +
-
+                    "gameID INT PRIMARY KEY," +
+                    "whiteUsername VARCHAR(255)," +
+                    "blackUsername VARCHAR(255)," +
+                    "gameName VARCHAR(255) NOT NULL," +
+                    "gameState TEXT NOT NULL," +
+                    "FOREIGN KEY (whiteUsername) REFERENCES users(username)," +
+                    "FOREIGN KEY (blackUsername) REFERENCES users(username))";
+            try (PreparedStatement stmt = conn.prepareStatement(createGamesTable)) {
+                stmt.executeUpdate();
+            }
         } catch (SQLException e) {
             throw new DataAccessException("Error creating tables: " + e.getMessage());
         }
