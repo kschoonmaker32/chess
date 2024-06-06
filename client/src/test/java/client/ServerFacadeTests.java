c3f56ff3a2ec52901a6eb0337a238b72eecede79
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-06 17:11:26 -0600

    added suggestions from phase 5 specs

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -4,16 +4,22 @@ import org.junit.jupiter.api.*;
 import server.Server;
 
 
+
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
+
+    //@BeforeEach
+    // clear database somehow
 
     @AfterAll
     static void stopServer() {
@@ -26,4 +32,11 @@ public class ServerFacadeTests {
         Assertions.assertTrue(true);
     }
 
+
+//    example register test
+//    @Test
+//    void register() throws Exception {
+//        var authData = facade.register("player1", "password", "p1@email.com");
+//        assertTrue(authData.authToken().length() > 10);
+    }
 }
