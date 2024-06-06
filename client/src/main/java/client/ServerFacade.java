bd8b52ec4a33bfaf6fe14161bd3506aaaed650c8
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-06 17:18:28 -0600

    added register method that returns authData object

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -22,4 +22,26 @@ public class ServerFacade {
 
 
     }
+
+    public AuthData register(String username, String password, String email) throws IOException {
+        URL url = new URL("http://" + serverHost + ":" + serverPort + "/register");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-type", "application/json");
+
+        UserData userData = new UserData(username, password, email);
+        String requestBody = gson.toJson(userData);
+
+        try (OutputStream os = connection.getOutputStream()) {
+            os.write(requestBody.getBytes());
+            os.flush();
+        }
+
+        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            return gson.fromJson(new InputStreamReader(connection.getInputStream()), AuthData.class);
+        } else {
+            throw new IOException("Error registering user" + connection.getResponseMessage());
+        }
+    }
 }
