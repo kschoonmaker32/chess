fc239df4c33cf0c2c3886d78674574b8d3485a2f
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-10 05:55:43 -0600

    got rid of unused code

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -100,19 +100,4 @@ public class DrawBoard {
             return BLACK_PIECE_COLOR;
         }
     }
-
-    private int[] convertPositionToIndices(String position) {
-        if (position.length() != 2) {
-            return null;
-        }
-        char column = position.charAt(0);
-        char row = position.charAt(1);
-
-        int colIndex = column - 'a';
-        int rowIndex = row - 1;
-        if (colIndex < 0 || colIndex > 7 || rowIndex < 0 || rowIndex > 7) {
-            return null;
-        }
-        return new int[]{rowIndex, colIndex};
-    }
 }
