90ba2ce0e30fa843022d15a32d12de5eab84aeb8
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-10 15:34:19 -0600

    implemented connectcommand class

--- shared/src/main/java/websocket/commands/ConnectCommand.java
+++ shared/src/main/java/websocket/commands/ConnectCommand.java
@@ -1,5 +1,16 @@
 package websocket.commands;
 
-public class ConnectCommand  {
+public class ConnectCommand extends UserGameCommand {
 
+    private final int gameID;
+
+    public ConnectCommand(String authToken, int gameID) {
+        super(authToken);
+        this.commandType = CommandType.CONNECT;
+        this.gameID = gameID;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
 }
