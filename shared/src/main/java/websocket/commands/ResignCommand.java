f3c9aa0cc283ff77454549bcee209ddda0f81981
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-10 15:42:16 -0600

    implemented resigncommand class

--- shared/src/main/java/websocket/commands/ResignCommand.java
+++ shared/src/main/java/websocket/commands/ResignCommand.java
@@ -1,4 +1,15 @@
 package websocket.commands;
 
-public class ResignCommand {
+public class ResignCommand extends UserGameCommand {
+    private final int gameID;
+
+    public ResignCommand(String authToken, int gameID) {
+        super(authToken);
+        this.commandType = CommandType.RESIGN;
+        this.gameID = gameID;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
 }
