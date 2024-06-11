79fc7cb4bde21f434ab91e20a9d3b9c02b0c1108
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-11 15:55:25 -0600

    trying to implement notification methods

--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -1,14 +1,22 @@
 package server;
 
 import com.google.gson.Gson;
+import model.GameData;
 import spark.Session;
 import websocket.commands.ConnectCommand;
 import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.NotificationMessage;
 
 import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.Set;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class WebSocketHandler {
     private static final Map<Integer, Set<Session>> gameSessions = new ConcurrentHashMap<>();
@@ -29,5 +37,52 @@ public class WebSocketHandler {
         }
     }
 
-    public void
+    private void handleConnect(Session session, String authToken, int gameID) throws IOException {
+        gameSessions.computeIfAbsent(gameID, k -> ConcurrentHashMap.newKeySet()).add(session);
+        sendNotificationToOthers(gameID, session, authToken + " connected to game " + gameID);
+
+    }
+
+    private void sendLoadGameMessage(int gameID) throws IOException {
+    }
+
+    private void sendNotificationToOthers(int gameID, Session senderSession, String notification) throws IOException {
+        NotificationMessage notificationMessage = new NotificationMessage(notification);
+        String message = gson.toJson(notificationMessage);
+
+        sendToOthers(gameID, senderSession, message);
+    }
+
+    private void sendToOthers(int gameID, Session senderSession, String message) throws IOException {
+        Set<Session> sessions = gameSessions.get(gameID);
+        if (sessions != null) {
+            for (Session session : sessions) {
+                if (!session.equals(senderSession)) {
+                    session.getBasicRemote().sendText(message);
+                }
+            }
+        }
+    }
+
+    private void sendNotificationToAll(int gameID, String notification) throws IOException {
+        NotificationMessage notificationMessage = new NotificationMessage(notification);
+        String message = gson.toJson(notificationMessage);
+
+        sendToAll(gameID, message);
+    }
+
+    private void sendToAll(int gameID, String message) throws IOException {
+        Set<Session> sessions = gameSessions.get(gameID);
+        if(sessions != null) {
+            for (Session session : sessions) {
+                session.getBasicRemote().sendText(message);
+            }
+        }
+    }
+
+    private void sendError(Session session, String errorMessage) throws IOException {
+        ErrorMessage errorMessageObj = new ErrorMessage(errorMessage);
+        session.getBasicRemote().sendText(errorMessageObj);
+    }
+
 }
