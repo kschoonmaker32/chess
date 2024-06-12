fa536e7a3374dfb8dc57d6bf5d4461e80715459f
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-12 17:14:12 -0600

    added onopen, message, error, and close methods

---
+++ client/src/main/java/client/WSFacade.java
@@ -0,0 +1,45 @@
+package client;
+
+import com.google.gson.Gson;
+import websocket.commands.ConnectCommand;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class WSFacade {
+    private Session session;
+    private final Gson gson;
+    private final BlockingDeque<String> messageQueue;
+
+    public WSFacade(String serverURI) throws URISyntaxException, DeploymentException, IOException {
+        this.gson = new Gson();
+        this.messageQueue = new LinkedBlockingDeque<>();
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        container.connectToServer(this, new URI(serverURI));
+    }
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        messageQueue.offer(message);
+    }
+
+    @OnError
+    public void onError(Throwable throwable) {
+        throwable.printStackTrace();
+    }
+
+    @OnClose
+    public void onClose() {
+        this.session = null;
+    }
+
+}
