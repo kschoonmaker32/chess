773fb6d8854686b82240e5d6e22dc899ff50388f
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-13 12:31:02 -0600

    added leavegame method

--- client/src/main/java/ui/GamePlay.java
+++ client/src/main/java/ui/GamePlay.java
@@ -1,31 +1,44 @@
 package ui;
 
+import client.ServerFacade;
+import client.WebSocketFacade;
+
+import javax.websocket.WebSocketContainer;
 import java.util.Scanner;
 
 public class GamePlay {
-
+    private final ServerFacade serverFacade;
+    private final WebSocketFacade webSocketFacade;
     private final Scanner scanner;
+    private final String authToken;
+    private final int gameID;
 
-    public GamePlay() {
+
+    public GamePlay(ServerFacade serverFacade, WebSocketFacade webSocketFacade, String authToken, int gameID) {
+        this.serverFacade = serverFacade;
+        this.webSocketFacade = webSocketFacade;
         this.scanner = new Scanner(System.in);
+        this.authToken = authToken;
+        this.gameID = gameID;
     }
 
     public void display() {
         boolean running = true;
         while (running) {
-            System.out.println("Enter command: Help, Redraw Chess Board, Leave, Make move, Resign, Highlight Legal Moves. ");
+            System.out.println("Enter command: Help, Redraw, Leave, Make move, Resign, Highlight Moves. ");
             String command = scanner.nextLine().trim().toLowerCase();
             switch (command) {
                 case "help":
                     displayHelp();
                     break;
-                case "redraw chess board":
+                case "redraw":
                 case "leave":
+                    leaveGame();
                     running = false;
                     break;
                 case "make move":
                 case "resign":
-                case "highlight legal moves":
+                case "highlight moves":
                 default:
                     System.out.println("Command not recognized. Type 'Help' for a list of available commands. ");
                     break;
@@ -34,12 +47,20 @@ public class GamePlay {
     }
 
     public void displayHelp() {
-        System.out.println("Redraw Chess Board: redraw the current chessboard. ");
+        System.out.println("Redraw: redraw the current chessboard. ");
         System.out.println("Leave: leave game.");
         System.out.println("Make move: make a move when it's your turn. ");
         System.out.println("Resign: Forfeit the game. ");
-        System.out.println("Highlight legal moves: highlight the possible moves for a piece of your choice. ");
+        System.out.println("Highlight moves: highlight the possible moves for a piece of your choice. ");
     }
 
+    public void leaveGame() {
+        try {
+            webSocketFacade.sendLeave(authToken, gameID);
+            System.out.println("Left the game. ");
+        } catch (Exception e) {
+            System.out.println("Failed to leave the game: " + e.getMessage());
+        }
+    }
 
 }
