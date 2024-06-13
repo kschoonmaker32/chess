6e5fba2ec47a593252b7acc63eabebf7fff3552c
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-13 09:39:39 -0600

    implemented display and displayhelp methods

--- client/src/main/java/ui/GamePlay.java
+++ client/src/main/java/ui/GamePlay.java
@@ -1,4 +1,40 @@
 package ui;
 
+import java.util.Scanner;
+
 public class GamePlay {
+
+    private final Scanner scanner;
+
+    public GamePlay() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void display() {
+        boolean running = true;
+        while (running) {
+            System.out.println("Enter command: Help, Redraw Chess Board, Leave, Make move, Resign, Highlight Legal Moves. ");
+            String command = scanner.nextLine().trim().toLowerCase();
+            switch (command) {
+                case "help":
+                    displayHelp();
+                    break;
+                case "redraw chess board":
+                case "leave":
+                case "make move":
+                case "resign":
+                case "highlight legal moves":
+            }
+        }
+    }
+
+    public void displayHelp() {
+        System.out.println("Redraw Chess Board: redraw the current chessboard. ");
+        System.out.println("Leave: leave game.");
+        System.out.println("Make move: make a move when it's your turn. ");
+        System.out.println("Resign: Forfeit the game. ");
+        System.out.println("Highlight legal moves: highlight the possible moves for a piece of your choice. ");
+    }
+
+
 }
