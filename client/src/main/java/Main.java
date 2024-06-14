284ae5b9f43447f3652c9a6b7b9723327505c991
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-14 17:29:50 -0600

    added try/catch block for wsfacade

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -6,7 +6,14 @@ import ui.PreLogin;
 public class Main {
     public static void main(String[] args) {
         var newServerFacade =  new ServerFacade("localhost", "8080");
-        var newWSFacade = new WebSocketFacade("ws://localhost:8080/ws"); // check here
+        WebSocketFacade newWSFacade = null;
+        try {
+            newWSFacade = new WebSocketFacade("ws://localhost:8080/ws"); // check here
+        } catch (Exception e) {
+            System.out.println("Failed to initialize WebSocket connection: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
         var preloginUI = new PreLogin(newServerFacade, newWSFacade);
         preloginUI.display();
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
