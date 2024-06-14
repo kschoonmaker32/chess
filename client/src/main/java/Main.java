df97d9c2763367dde2f2f5bb6d307dbdfa5b3fb5
kschoonmaker32 <kendra.l.schoonmaker@gmail.com>
2024-06-13 18:04:35 -0600

    check comment

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -6,7 +6,7 @@ import ui.PreLogin;
 public class Main {
     public static void main(String[] args) {
         var newServerFacade =  new ServerFacade("localhost", "8080");
-        var newWSFacade = new WebSocketFacade("ws://localhost:8080/ws");
+        var newWSFacade = new WebSocketFacade("ws://localhost:8080/ws"); // check here
         var preloginUI = new PreLogin(newServerFacade, newWSFacade);
         preloginUI.display();
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
